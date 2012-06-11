b10c9c06bc7efe3a7594b25c883502b2227f4656
natjohns <natjohns@c58b5278-a2c8-4678-9377-d879b98ccb7f>
2012-06-11 13:29:31 +0000

    MOBILITY-211

---
+++ tours/api/src/main/java/org/kuali/mobility/tours/dao/ToursDao.java
@@ -0,0 +1,19 @@
+package org.kuali.mobility.tours.dao;
+
+import java.util.List;
+
+import org.kuali.mobility.tours.entity.POI;
+import org.kuali.mobility.tours.entity.Tour;
+
+public interface ToursDao {
+	public Tour findTourById(Long id);
+    public Tour findTourByName(String name);
+    public Long saveTour(Tour tour);
+    public List<Tour> findAllTours();
+    public void deleteTourById(Long id);
+	public POI findPoiById(Long id);
+	public Long savePoi(POI poi);
+	public void deletePoiById(Long poiId);
+	public List<POI> findAllCommonPOI();
+	public POI findPoiByOrder(Long tourId, Integer order);
+}
