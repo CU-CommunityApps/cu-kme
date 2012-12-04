29a68c7beca890fc115b71a066b7cff03c034a2e
sedgar <sedgar@c58b5278-a2c8-4678-9377-d879b98ccb7f>
2012-12-04 19:56:40 +0000

    Merge in Kevin's datasource changes. Remove several helper classes that are no longer needed.

--- dining/impl/src/main/java/org/kuali/mobility/dining/dao/DiningDaoCUImpl.java
+++ dining/impl/src/main/java/org/kuali/mobility/dining/dao/DiningDaoCUImpl.java
@@ -28,13 +28,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
 import org.apache.log4j.Logger ;
 import org.kuali.mobility.dining.entity.Place ;
-import org.kuali.mobility.dining.helpercu.DiningDb ;
 import org.kuali.mobility.dining.helpercu.DiningResult;
 import org.kuali.mobility.dining.helpercu.Meals;
 import org.kuali.mobility.util.mapper.DataMapper;
@@ -43,13 +38,12 @@ import flexjson.JSONSerializer ;
 
 /**
  * We are kind of half-stepping it here, for now. The Cornell "place list" info is 
- * hard coded in the XML file. as we do not have a datasource for this 
+ * hard coded in the XML file, as we do not have a datasource for this 
  * handy right now. But this will be enough to get us launched.
  * 
  * The Cornell meal and menu data is live, and comes from an Oracle table.
  *
  */
-/* --------------------------------------------------------------------- */
 public class DiningDaoCUImpl implements DiningDao {
 
 	private static final Logger LOG = Logger.getLogger( DiningDaoCUImpl.class );
@@ -61,14 +55,12 @@ public class DiningDaoCUImpl implements DiningDao {
 	private String placeMappingFile;
 	private DataSource diningDataSource;
 
-	//private String datasourceUrl ;
-	//private String datasourceUsername ;
-	//private String datasourcePassword ; 
-
-	//@PersistenceContext(unitName = "dining-unit")
-	//private EntityManager entityManager;
-
-	/* --------------------------------------------------------------------- */
+	/*
+	 * (non-Javadoc)
+	 * @see org.kuali.mobility.dining.dao.DiningDao#getMenusJson(java.lang.String, java.lang.String)
+	 * 
+	 * We are not using the "location" parameter, as I did not want to change the interface.
+	 */
 	public String getMenusJson (final String name, String location) {
 
 		String sSql = "select meal, course, formal_name, eating_well_flag from dining.menudetailweb where service_unit = ? and eventdate = ?" ;
@@ -77,35 +69,27 @@ public class DiningDaoCUImpl implements DiningDao {
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		String sDate = df.format(dt) ;
 
+		ArrayList<DiningResult> alDr = new ArrayList<DiningResult>() ;
+
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
-		Meals mls = new Meals() ;
-
 		try {
-			System.out.println("Creating connection.");
 			conn = diningDataSource.getConnection();
-			System.out.println("Creating statement.");
 			stmt = conn.prepareStatement(sSql);
-
-			stmt.setString(1, location) ;
+			stmt.setString(1, name) ;
 			stmt.setString(2, sDate) ;
-			System.out.println("Executing statement.");
 			rs = stmt.executeQuery();
-		
-
-			DiningResult dr;
 
 			while(rs.next()) {
-			//for (Object[] rs : rset) {
-				dr = new DiningResult() ;
+				DiningResult dr = new DiningResult() ;
 				dr.setMeal(rs.getString(1)) ;
 				dr.setStation(rs.getString(2)) ;
 				dr.setEntree(rs.getString(3)) ;
-				dr.setEatingWellFlag(rs.getString(4)); 
-				mls.appendMeal(dr) ;
+				dr.setEatingWellFlag(rs.getString(4));
+			
+				alDr.add(dr) ;				
 			}
-			//	}
 		} catch(SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -113,14 +97,18 @@ public class DiningDaoCUImpl implements DiningDao {
 			try { stmt.close(); } catch(Exception e) { }
 			try { conn.close(); } catch(Exception e) { }
 		}
-
-	
+				
+		Collections.sort(alDr) ;	// this will make the meals display nicely
+		Meals mls = new Meals() ;		
+		for (DiningResult dr :alDr)
+			{
+			mls.appendMeal(dr) ;			
+			}
 
 		String sFlexJson = new JSONSerializer().exclude("*.class").deepSerialize(mls) ;
 		return (sFlexJson) ;
 	}
 
-	/* --------------------------------------------------------------------- */
 	private void initPlaceList () {
 
 		if (placeList==null)
@@ -135,68 +123,26 @@ public class DiningDaoCUImpl implements DiningDao {
 		}
 	}
 
-	/* ------------------------------------------------------------------------- */
-	//public String getDatasourceUrl() {
-	//
-	//return datasourceUrl;
-	//}
-	//
-	///* ------------------------------------------------------------------------- */
-	//public void setDatasourceUrl(String datasourceUrl) {
-	//
-	//this.datasourceUrl = datasourceUrl;
-	//}
-	//
-	///* ------------------------------------------------------------------------- */
-	//public String getDatasourceUsername() {
-	//
-	//return datasourceUsername;
-	//}
-	//
-	///* ------------------------------------------------------------------------- */
-	//public void setDatasourceUsername(String datasourceUsername) {
-	//
-	//this.datasourceUsername = datasourceUsername;
-	//}
-	//
-	///* ------------------------------------------------------------------------- */
-	//public String getDatasourcePassword() {
-	//
-	//return datasourcePassword;
-	//}
-	//
-	///* ------------------------------------------------------------------------- */
-	//public void setDatasourcePassword(String datasourcePassword) {
-	//
-	//this.datasourcePassword = datasourcePassword;
-	//}
-
-	/* ------------------------------------------------------------------------- */
-
 	public String getPlaceSourceUrl() {
 
 		return placeSourceUrl;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public void setPlaceSourceUrl(String placeSourceUrl) {
 
 		this.placeSourceUrl = placeSourceUrl;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public String getPlaceMappingFile() {
 
 		return placeMappingFile;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public void setPlaceMappingFile(String placeMappingFile) {
 
 		this.placeMappingFile = placeMappingFile;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public List<Place> getPlaceList() {
 
 		if (placeList==null || placeList.isEmpty()) 
@@ -205,25 +151,18 @@ public class DiningDaoCUImpl implements DiningDao {
 		return placeList;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public void setMapper(DataMapper mapper) {
 
 		this.mapper = mapper;
 	}
 
 	public DataSource getDiningDataSource() {
+	
 		return this.diningDataSource;
 	}
 
 	public void setDiningDataSource(DataSource diningDataSource) {
+	
 		this.diningDataSource = diningDataSource;
 	}
-
-	//public EntityManager getEntityManager() {
-	//    return this.entityManager;
-	//}
-	//
-	//public void setEntityManager(EntityManager entityManager) {
-	//    this.entityManager = entityManager;
-	//}
 }
--- dining/impl/src/main/java/org/kuali/mobility/dining/helpercu/DiningResult.java
+++ dining/impl/src/main/java/org/kuali/mobility/dining/helpercu/DiningResult.java
@@ -1,43 +1,16 @@
 package org.kuali.mobility.dining.helpercu ;
 
-import javax.persistence.NamedQuery;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-import java.util.Date;
-
 /**
  * Holds the elements we need from result set, from the query 
  * to the Cornell Dining database.
  */
-/* ------------------------------------------------------------------------- */
-@Entity
-@Table(name = "menudetailweb", catalog = "dining")
-@NamedQuery(name="lookupMealsForToday", query="Select meal, course, formal_name, eating_well_flag from DiningResult where service_unit = :service_unit and eventdate = :eventdate")
 public class DiningResult implements Comparable <DiningResult> {
 
-	//String sSql = "select meal, course, formal_name, eating_well_flag from dining.menudetailweb where service_unit = ? and eventdate = ?" ;
-
-
-	@Column(name = "meal")
 	private String meal = "" ;
-	
-	@Column(name = "course")
 	private String station = "" ;
-	
-	@Column(name = "formal_name")
 	private String entree = "" ;
-	
-	@Column(name = "eating_well_flag")
 	private String eatingWellFlag = "" ;
-	
-	@Column(name= "service_unit")
 	private String serviceUnit = "";
-	
-	
-	private Date eventdate;
-
 	private String mealRanking[] = {"breakfast", "brunch", "lunch", "dinner" } ;
 	
 	/**
@@ -57,7 +30,6 @@ public class DiningResult implements Comparable <DiningResult> {
 		return (iReturn) ;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	private int getMealRanking (String sMeal) {
 
 		int iMealRankSize = mealRanking.length ;
@@ -77,49 +49,41 @@ public class DiningResult implements Comparable <DiningResult> {
 		return (iRank) ;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public String getMeal() {
 
 		return meal;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public void setMeal(String meal) {
 
 		this.meal = noNullAndTrimAndUgh(meal);
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public String getStation() {
 
 		return station;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public void setStation(String station) {
 
 		this.station = noNullAndTrimAndUgh(station);
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public String getEntree() {
 
 		return entree;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public void setEntree(String entree) {
 
 		this.entree = noNullAndTrimAndUgh(entree);
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public String getEatingWellFlag() {
 
 		return eatingWellFlag;
 	}
 
-	/* ------------------------------------------------------------------------- */
 	public void setEatingWellFlag(String eatingWellFlag) {
 
 		this.eatingWellFlag = noNullAndTrimAndUgh(eatingWellFlag);
@@ -133,15 +97,6 @@ public class DiningResult implements Comparable <DiningResult> {
 		this.serviceUnit = serviceUnit;
 	}
 	
-	public Date getEventdate() {
-		return eventdate;
-	}
-	
-	public void setEventdate(Date eventdate) {
-		this.eventdate = eventdate;
-	}
-
-	/* ------------------------------------------------------------------------- */
 	private String noNullAndTrimAndUgh (String sValue) {
 
 		if (sValue == null)
@@ -155,7 +110,4 @@ public class DiningResult implements Comparable <DiningResult> {
 
 		return (sValue) ;
 	}
-	
-	
-
 }
